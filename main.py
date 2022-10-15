d8708c389f573eb877903967e9ae52d41c2262d8
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-10-15 12:59:31 +0300

    init

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+# create the extension
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
 
 
 @app.route('/')
 def main_page():
-    page_title = 'Beaver_knithen: главная страница'
-    beaver_knithen_main_1 = "Beaver_knithen"
-    beaver_knithen_main_2 = "Вязанные изделия любой сложности из наличия и на заказ"
+    title = 'Аптека'
+    page_name = 'главная страница'
+    description = 'Вязанные изделия любой сложности из наличия и на заказ'
+    submenu = [
+        {'title': 'Выбрать модель'},
+        {'title': 'Заказать из наличия'},
+        {'title': 'Помощь специалиста'},
+    ]
+    menu = [
+        {'title': 'Доставка и оплата'},
+        {'title': 'О нас'},
+        {'title': 'Контакты'},
+        {'title': 'Каталог', 'submenu': submenu},
+    ]
+    filters = [
+        {'name': 'Цвет'},
+        {'name': 'Шерсть'},
+        {'name': 'Пошив'},
+        {'name': 'Узоры'},
+        {'name': 'Пол'},
+        {'name': 'Размер'},
+        {'name': 'Страна'},
+        {'name': 'Забрать из наличия'},
+    ]
     context = {
-        'title': page_title,
-        'beaver_main_1': beaver_knithen_main_1,
-        'beaver_main_2': beaver_knithen_main_2,
+        'title': title,
+        'page_name': page_name,
+        'description': description,
+        'menu': menu,
+        'filters': filters,
     }
-    return render_template('main_page.html', **context)
+    return render_template('main.html', **context)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
